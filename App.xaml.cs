ec8e012176001fa8c9cc9c6bb5e9c39d27271a8b
Intellias\kmalovanyk <konstiantyn.malovanyk@intellias.com>
2024-11-19 01:45:52 +0200

    Implemented simple error handling and some logic for that.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
 namespace Quipu_Task
 {
@@ -10,5 +8,4 @@ namespace Quipu_Task
     public partial class App : Application
     {
     }
-
 }
---
+++ Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Quipu_Task.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? Visibility.Visible : Visibility.Hidden;
+            }
+            return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is Visibility visibility && visibility == Visibility.Visible;
+        }
+    }
+}
